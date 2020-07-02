3ce8865c4443e88cd1d1b1ff6d4a9297bc0b79e6
Jeffery Allain <jefferyallain@gmail.com>
2020-07-02 15:07:46 -0500

    Added ExchangeTable header rebuild

--- PortRoller.js
+++ PortRoller.js
@@ -36,6 +36,42 @@ function BuildExchangeTable(source) {
 }
 
 function BuildExchangeTableHeaders(table) {
+    var row1 = table.insertRow(0);
+    row1.className = "header";
+    var cell1 = row1.insertCell(0);
+    cell1.innerHTML = "Buy";
+    cell1.colSpan = 3;
+    cell1.className = "buy";
+    var cell2 = row1.insertCell(1);
+    var cell2 = row1.insertCell(2);
+    cell2.innerHTML = "Sell";
+    cell2.colSpan = 3;
+    cell2.className = "sell";
+
+    var row2 = table.insertRow(1);
+    row2.className = "header headerBorder";
+    var cell1_2 = row2.insertCell(0);
+    cell1_2.innerHTML = "Ton";
+    cell1_2.className = "buy";
+    var cell2_2 = row2.insertCell(1);
+    cell2_2.innerHTML = "Pound";
+    cell2_2.className = "buy";
+    var cell3_2 = row2.insertCell(2);
+    cell3_2.innerHTML = "Ounce";
+    cell3_2.className = "buy";
+
+    var cell4_2 = row2.insertCell(3);
+    cell4_2.innerHTML = "Item";
+
+    var cell5_2 = row2.insertCell(4);
+    cell5_2.innerHTML = "Ton";
+    cell5_2.className = "sell";
+    var cell6_2 = row2.insertCell(5);
+    cell6_2.innerHTML = "Pound";
+    cell6_2.className = "sell";
+    var cell7_2 = row2.insertCell(6);
+    cell7_2.innerHTML = "Ounce";
+    cell7_2.className = "sell";
 }
 
 // Handlers
