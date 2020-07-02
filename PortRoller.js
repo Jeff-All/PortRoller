9e6731da8069ccc5e13370a4827dd374834fbb9b
Jeffery Allain <jefferyallain@gmail.com>
2020-07-02 12:15:31 -0500

    Added PortData Processing loop and Sources building

--- PortRoller.js
+++ PortRoller.js
@@ -1,3 +1,23 @@
 
 var ButtonTable = $('#ButtonTable');
 var ExchangeTable = $('#ExchangeTable');
+var Sources=[];
+
+for (const [key, value] of Object.entries(PortData)) {
+    ProcessEntry(key, value)
+}
+
+console.log(`Sources:${Sources}`);
+
+function ProcessEntry(key, entry) {
+    console.log(`ProcessEntry(${key}):${entry.Sources}`);
+    AddSources(entry.Sources);
+}
+
+function AddSources(sources) {
+    sources.forEach(source => {
+        if(!Sources.includes(source)){
+            Sources.push(source);
+        }
+    });
+}
