97aaf50fbc8fa2682760cf3c0031f0cdf9b603e5
Jeffery Allain <jefferyallain@gmail.com>
2020-07-02 11:55:40 -0500

    Removing Logging Message

--- PortRoller.js
+++ PortRoller.js
@@ -1,13 +1,3 @@
 
 var ButtonTable = $('#ButtonTable');
 var ExchangeTable = $('#ExchangeTable');
-
-console.log(`PortData:${PortData}`)
-
-// Load PortData
-
-// $.getJSON("PortData.json",function(json){
-//     console.log(`json:${json}`)
-//     PortData = json;
-//     console.log(`PortData:${PortData}`)
-// });
